a4ba06126d787ec40256faf244acaa0da0e944f9
niuwenxu <niuwx@jideos.com>
2020-04-07 11:28:00 +0800

    Curl resume downloads

--- video/trainval/download.sh
+++ video/trainval/download.sh
@@ -3,5 +3,5 @@ ava_url="https://s3.amazonaws.com/ava-dataset/trainval/"
 while read p; do
   echo $p
   echo $ava_url$p -O;
-  curl $ava_url$p -O;
+  curl $ava_url$p -C - -O;
 done <ava_file_names_trainval_v2.1.txt
